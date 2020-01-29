ea7a9e9bbacad87da44a1e47d71880e480373f85
kennyjijet <usistal@hotmail.com>
2020-01-29 15:40:03 +0700

    Add files via upload

---
+++ Linklist/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+
+namespace Linklist
+{
+    public class Node
+    {
+        internal int data;
+        internal Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public class DNode
+    {
+        internal int data;
+        internal DNode prev;
+        internal DNode next;
+        public DNode(int d)
+        {
+            data = d;
+            prev = null;
+            next = null;
+        }
+    }
+
+    public class DoubleLinkedList
+    {
+        public DNode head;
+        public void InsertFront(DoubleLinkedList doubleLinkedList, int data)
+        {
+            DNode newNode = new DNode(data);
+            newNode.next = doubleLinkedList.head;
+            newNode.prev = null;
+            if (doubleLinkedList.head != null)
+            {
+                doubleLinkedList.head.prev = newNode;
+            }
+            doubleLinkedList.head = newNode;
+        }
+
+        public void InsertLast(DoubleLinkedList doubleLinkedList, int data)
+        {
+            DNode newNode = new DNode(data);
+            if (doubleLinkedList.head == null)
+            {
+                newNode.prev = null;
+                doubleLinkedList.head = newNode;
+                return;
+            }
+            DNode lastNode = GetLastNode(doubleLinkedList);
+            lastNode.next = newNode;
+            newNode.prev = lastNode;
+        }
+
+        public void InsertAfter(DNode prev_node, int data)
+        {
+            if (prev_node == null)
+            {
+                Console.WriteLine("The given prevoius node cannot be null");
+                return;
+            }
+            DNode newNode = new DNode(data);
+            newNode.next = prev_node.next;
+            prev_node.next = newNode;
+            newNode.prev = prev_node;
+            if (newNode.next != null)
+            {
+                newNode.next.prev = newNode;
+            }
+        }
+
+        public void DeleteNodebyKey(DoubleLinkedList doubleLinkedList, int key)
+        {
+            DNode temp = doubleLinkedList.head;
+            if (temp != null && temp.data == key)
+            {
+                doubleLinkedList.head = temp.next;
+                doubleLinkedList.head.prev = null;
+                return;
+            }
+            while (temp != null && temp.data != key)
+            {
+                temp = temp.next;
+            }
+            if (temp == null)
+            {
+                return;
+            }
+            if (temp.next != null)
+            {
+                temp.next.prev = temp.prev;
+            }
+            if (temp.prev != null)
+            {
+                temp.prev.next = temp.next;
+            }
+        }
+
+
+        public DNode deleteNode(DNode head_ref, DNode del)
+        {
+            // base case  
+            if (head_ref == null || del == null)
+                return null;
+
+            // If node to be deleted is head node  
+            if (head_ref == del)
+                head_ref = del.next;
+
+            // Change next only if node to be  
+            // deleted is NOT the last node  
+            if (del.next != null)
+                del.next.prev = del.prev;
+
+            // Change prev only if node to be  
+            // deleted is NOT the first node  
+            if (del.prev != null)
+                del.prev.next = del.next;
+
+            return head_ref;
+        }
+
+        // function to delete all nodes from the  
+        // doubly linked list divisible by K  
+        public DNode deleteDivisibleNodes(DNode head_ref, int K)
+        {
+            DNode ptr = head_ref;
+            DNode next;
+
+            while (ptr != null)
+            {
+                next = ptr.next;
+
+                // if true, delete node 'ptr'  
+                if (ptr.data % K == 0)
+                    deleteNode(head_ref, ptr);
+                ptr = next;
+            }
+            return head_ref;
+        }
+        public DNode GetLastNode(DoubleLinkedList doubleLinkedList)
+        {
+            DNode temp = doubleLinkedList.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+
+    public class SingleLinkedList
+    {
+        public Node head;
+
+        public void InsertFront(SingleLinkedList singlyList, int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = singlyList.head;
+            singlyList.head = new_node;
+        }
+
+        public void InsertLast(SingleLinkedList singlyList, int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (singlyList.head == null)
+            {
+                singlyList.head = new_node;
+                return;
+            }
+            Node lastNode = GetLastNode(singlyList);
+            lastNode.next = new_node;
+        }
+
+        public void InsertAfter(Node prev_node, int new_data)
+        {
+            if (prev_node == null)
+            {
+                Console.WriteLine("The given previous node Cannot be null");
+                return;
+            }
+            Node new_node = new Node(new_data);
+            new_node.next = prev_node.next;
+            prev_node.next = new_node;
+        }
+
+        public void DeleteNodebyKey(SingleLinkedList singlyList, int key)
+        {
+            Node temp = singlyList.head;
+            Node prev = null;
+            if (temp != null && temp.data == key)
+            {
+                singlyList.head = temp.next;
+                return;
+            }
+            while (temp != null && temp.data != key)
+            {
+                prev = temp;
+                temp = temp.next;
+            }
+            if (temp == null)
+            {
+                return;
+            }
+            prev.next = temp.next;
+        }
+        public Node GetLastNode(SingleLinkedList singlyList)
+        {
+            Node temp = singlyList.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+        public void PrintList()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+
+
+        public void ReverseLinkedList(SingleLinkedList singlyList)
+        {
+            Node prev = null;
+            Node current = singlyList.head;
+            Node temp = null;
+            while (current != null)
+            {
+                temp = current.next;
+                current.next = prev;
+                prev = current;
+                current = temp;
+                //Console.WriteLine(prev.data);
+            }
+            singlyList.head = prev;
+            //Console.WriteLine(singlyList.head);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            SingleLinkedList sll = new SingleLinkedList();
+            sll.InsertFront(sll, 1);
+            sll.InsertFront(sll, 2);
+            sll.InsertFront(sll, 3);
+            //sll.DeleteNodebyKey(sll, 3);
+            sll.ReverseLinkedList(sll);
+            sll.PrintList();
+        }
+    }
+}
